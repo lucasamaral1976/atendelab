bd05f67a89c9d375c526f697c076e00a4c26702f
lucasamaral1976 <lucasbdoamarad@gmail.com>
2026-06-08 20:23:47 -0300

    atualização

--- routes.php
+++ routes.php
@@ -1,28 +1,47 @@
 <?php
 
 require_once 'config/database.php';
-require_once 'app/Controllers/UsuariosController.php';
 
-$controller = new UsuariosController($pdo);
+require_once 'app/Controllers/UsuariosController.php';
+require_once 'app/Controllers/ClientesController.php';
 
 $acao = $_GET['acao'] ?? '';
+
 switch ($acao) {
 
+    // USUÁRIOS
+
     case 'listar':
+        $controller = new UsuariosController($pdo);
         $controller->listar();
         break;
 
     case 'cadastrar':
+        $controller = new UsuariosController($pdo);
         $controller->cadastrar();
         break;
 
-        case 'atualizar':
-    $controller->atualizar();
-    break;
+    case 'atualizar':
+        $controller = new UsuariosController($pdo);
+        $controller->atualizar();
+        break;
 
     case 'excluir':
-    $controller->excluir();
-    break;
+        $controller = new UsuariosController($pdo);
+        $controller->excluir();
+        break;
+
+    // CLIENTES
+
+    case 'listar_clientes':
+        $controller = new ClientesController($pdo);
+        $controller->listar();
+        break;
+
+    case 'cadastrar_cliente':
+        $controller = new ClientesController($pdo);
+        $controller->cadastrar();
+        break;
 
     default:
         echo json_encode([
